0f5dc2ee71a1da60b5c8247f7c17f175757358e2
krischanthas <kris.chanthasiriphan@gmail.com>
2019-12-30 23:50:21 -0800

    added more content to the about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,8 +8,8 @@ export default function AboutUs() {
                               <h2>Our Story</h2>
                         </div>
                         <div className="textBox">
-                              <p> &emsp;Here at The Thai Guys, when it comes to our food, we are dedicated to delivering the type of authenticity that can only be matched by traveling to Thailand. Chef Chai strives to stay true to the five flavors of Thai cuisine, bringing the perfect balance of sweet, spicy, sour, savory, and bitter ingredients to make your mouth water instantly.</p>
-                              <p> &emsp;Our menu is designed to pay homage to Chai’s hometown and showcase the common Thai street foods he grew up with.  Our food is the culmination of our Thai-Chinese roots and we feel the need to share it with the world!</p>
+                              <p> &emsp;Here at The Thai Guys, when it comes to our food, we are dedicated to delivering the type of authenticity that can only be matched by traveling to Thailand. Chef Chai, owner and cook, strives to stay true to the five flavors of Thai cuisine by bringing the perfect balance of sweet, spicy, sour, savory, and bitter ingredients together in every dish.</p>
+                              <p> &emsp;Our menu is designed to pay homage to Chai’s hometown of Bankgok and aims to showcase the common Thai street foods he grew up eating. Our food is the culmination of our Thai-Chinese roots and we feel the need to share it with the world!</p>
                         </div>
 
             </div>
