b3797270213ed3a84ae936ad8da3c44211e0109d
krischanthas <kris.chanthasiriphan@gmail.com>
2020-01-08 15:01:30 -0800

    added carousel component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ export default function Menu() {
       return (
             <div className="menuBox" id="menu">
                   <div className="menu">
-                  <h2>Menu Coming Soon</h2>
+                  {/* <h2>Menu Coming Soon</h2> */}
                   </div>
             </div>
       )
