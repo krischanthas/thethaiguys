8a1cc7565c6a69d78aa0a79d547811afe6d22446
krischanthas <kris.chanthasiriphan@gmail.com>
2020-02-11 12:13:55 -0800

    updated h2 on Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,8 @@ export default function Contact() {
       return (
             <div className="contactBox" id="contact">
                   <div className="contact">
-                  <h2>Accepting Orders Soon</h2>
+                  <h2>Now Accepting Orders</h2>
+                  <div><h4>Available on all delivery platforms</h4></div>
                   <h3>(562) 414-4418</h3>
                   <h3>1388 Daisy Ave, Long Beach, CA 90813</h3>
                   <h3><a href="mailto:contact@thethaiguys.com">Contact us!</a></h3>
